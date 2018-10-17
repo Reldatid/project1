4fcc0e36e245e9877f84d8de926c20a9fc4815ce
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-10-17 15:08:53 +1100

    added root routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: "session#new"
+
   resources :users, except: [:index]
 
   get "/locations/new_universe" => "locations#new_universe";
